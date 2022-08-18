5b9778ab1f12f3ab38748a0079ff9c9aa022a0d8
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2022-08-18 17:38:44 -0300

    chore: adding typeDefs and resolvers

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 /* eslint-disable prettier/prettier */
-console.log('Hello world');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+    typeDefs: gql `
+    type Query {
+      hello: String
+      hi: String
+    }
+  `,
+    resolvers: {
+        Query: {
+            hello: () => {
+                return 'Hello again';
+            },
+            hi: () => {
+                return 'Hi people';
+            },
+        },
+    },
+});
+
+server.listen(4000).then(({ url }) => {
+    console.log(`Server listening on url ${url}`);
+});
