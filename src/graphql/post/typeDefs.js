48a56d16d01a596618f35e990992085a869213d6
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2022-08-22 12:16:59 -0300

    chore: adding union type

--- src/graphql/post/typeDefs.js
+++ src/graphql/post/typeDefs.js
@@ -3,8 +3,27 @@ import { gql } from 'apollo-server-core';
 
 export const postTypeDefs = gql`
   extend type Query {
-    post(id: ID!): Post!
+    post(id: ID!): PostResult!
     posts(data: ApiFiltersInput): [Post!]!
+  }
+
+  union PostResult = PostNotFound | PostTimeoutError | Post
+
+  interface PostError {
+    statusCode: Int!
+    message: String!
+  }
+
+  type PostNotFound implements PostError {
+    statusCode: Int!
+    message: String!
+    postId: String
+  }
+
+  type PostTimeoutError implements PostError {
+    statusCode: Int!
+    message: String!
+    timeout: Int!
   }
 
   type Post {
